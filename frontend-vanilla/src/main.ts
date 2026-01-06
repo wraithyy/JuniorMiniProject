b3b8662f516963f2a491c5385a5a495589d0e071
Zuzana Osobová <zuzana.osobova@gmail.com>
2026-01-06 16:24:18 +0100

    Mostyl CSS changes and some todos

--- frontend-vanilla/src/main.ts
+++ frontend-vanilla/src/main.ts
@@ -1,4 +1,5 @@
 import './styles/main.css';
+import * as stream from "node:stream";
 
 // TODO: Implementovat aplikaci pro správu kontaktů
 //
@@ -28,18 +29,42 @@ if (app) {
       console.log(e)
       console.log(form)
 
+      //TODO Opravit tuto část, zajistit správné typy dat
+
+      //Alternativa od Copilota, zajistí, že všechna data, která přijdou, tak jsou správná, případně je ošetří
+
+      /*const formEl = e.currentTarget as HTMLFormElement;
+      const fd = new FormData(formEl);
+
+      const data = {
+        firstName: (fd.get('firstName') as string) || '',
+        lastName: (fd.get('lastName') as string) || '',
+        email: (fd.get('email') as string) || '',
+        phone: (fd.get('phone') as string) || '',
+        note: (fd.get('note') as string) || '',
+        gender: (fd.get('gender') as string) || '',
+        city: (fd.get('city') as string) || '',
+        street: (fd.get('street') as string) || '',
+        houseNumber: (fd.get('houseNumber') as string) || '',
+        zipCode: (fd.get('zipCode') as string) || 0,
+        birthDate: (fd.get('birthDate') as string) || '',
+      };*/
+
+
+      //Problém: sice funkční, ale nazjišťuje, že opravdu přijdou správná data
       const data = {
         firstName: e.target.firstName.value,
         lastName: e.target.lastName.value,
         email: e.target.email.value,
-        phone: "",
-        note: "",
-        gender: "",
-        city: "",
-        street: "",
-        houseNumber: "",
-        zipCode: 0,
-        birthDate: "",
+        phone: e.target.phone.value,
+        note: e.target.note.value,
+        gender: e.target.gender.value,
+        city: e.target.city.value,
+        street: e.target.street.value,
+        houseNumber: e.target.houseNumber.value,
+        zipCode: e.target.zipCode.value,
+
+        //birthDate: e.target.birthDate.value //mám tu nějaký problém, že to nelze "cast to date"
       };
 
       console.log(data);
